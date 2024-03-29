8adb382ebbc23f8a39347868f66bae953455531a
Joseph Campbell <thecanadianjoe@gmail.com>
2024-03-29 10:15:49 -0400

    added array for cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 8
 let secondCard = 9
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,7 +10,11 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    renderGame() 
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
     message = "Do you want to draw another card?"
@@ -29,5 +34,6 @@ function startGame() {
 function newCard() {
     let card = 4
     sum += card
-    startGame()
+    cards.push(card)
+    renderGame()
 }
